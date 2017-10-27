408841f3cc61e5f619ee46d67e77218db45f8094
kagose yusuke <>
2017-10-27 17:17:06 +0900

    added validation user and listing

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,4 +2,8 @@ class Listing < ApplicationRecord
   has_attached_file :image, styles: { medium: "200x>", thumb: "                100x100>" }, default_url: "default.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
+  validates :name, :description, :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates_attachment_presence :image
+
 end
