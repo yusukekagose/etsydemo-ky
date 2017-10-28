8e36af3747e04e2ce892b9d0230b71c24953266f
kagose yusuke <>
2017-10-28 10:56:03 +0900

    added order history page

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
 
+  root 'listings#index'
+
   devise_for :users
   resources :listings do
-    resources :orders
+    resources :orders, only: [:new, :create]
   end
   get "pages/about"
   get "pages/contact"
   get "seller" => "listings#seller"
+  get 'sales' => "orders#sales"
+  get 'purchases' => "orders#purchases"
 
-  root 'listings#index'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
